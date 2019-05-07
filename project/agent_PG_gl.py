457136e4b840d4dbaa5fe27747b32fe6f2303222
Aubry Julie <43893665+AubryJulie@users.noreply.github.com>
2019-05-07 21:48:33 +0200

    Add files via upload

--- project/agent_PG_gl.py
+++ project/agent_PG_gl.py
@@ -39,13 +39,13 @@ class Continuous_Agent:
 		action = np.random.normal(mean, self.std)
 		return mean, action
 		
-	def Dlog(self, reward_sum, episode_mean, episode_action, episode_state):
+	def Dlog(self, reward_sum_discount, episode_mean, episode_action, episode_state):
 		"""
 		return the sum of the derivative of the log policy multiply by the sum of reward.
 		"""
 		# compute the difference between the action and the mean of the gaussian model divide by the 
 		# variance and multiply by the sum of reward
-		dif = [(x1 - x2)/(self.std**2)*reward_sum/len(episode_action) for (x1, x2) in zip(episode_action, episode_mean)]
+		dif = [(x1 - x2)/(self.std**2)*reward_sum_discount/len(episode_action) for (x1, x2) in zip(episode_action, episode_mean)]
 		dlog = [a*b for a,b in zip(episode_state,dif)]
 		# sum for all weights
 		dlog_array = np.asarray(dlog)
@@ -59,39 +59,45 @@ class Continuous_Agent:
 		mean = np.mean(dlog, axis=0)
 		self.theta = [x1 + self.learning_rate*x2 for (x1, x2) in zip(self.theta, mean)]
 		
-	def evaluate(self, EPISODE_NUMBER = 50, TIME_LIMITE=1000):
+	def evaluate(self, EPISODE_NUMBER = 50, TIME_LIMITE=10000):
 		"""
 		Evaluate the model: return the mean of cumulative reward 
 		"""
 		reward_sum = 0
 		episode_number = 0
 		previous_observation = self.env.reset()
-		sum_reward_list = []
+		sum_reward_list = [] 
+		time = 0
 		while episode_number != EPISODE_NUMBER:
 			# give the mean and action given an obervation.
 			mean, action = self.step(previous_observation)
 			# give the observation and reward given an action.
 			observation, reward, done = self.env.step([action])
-			reward_sum += reward 
-			time = 0
+			reward_sum += reward
+			time += 1
 			if done or time > TIME_LIMITE:
 				sum_reward_list.append(reward_sum)
 				reward_sum = 0
 				episode_number += 1
 				previous_observation = self.env.reset()	
+				time = 0
+			previous_observation = observation
 		sum_reward_array = np.asarray(sum_reward_list)
 		return np.mean(sum_reward_array), np.std(sum_reward_array)
 		
-	def train(self, EPISODE_NUMBER = 1000, batch_size = 20, TIME_LIMITE = 1000):
+	def train(self, EPISODE_NUMBER = 2000, batch_size = 20, TIME_LIMITE = 10000):
+		"""
+		Train the model.
+		"""
 		# Init
 		episode_rewards, episode_state, episode_action, episode_mean =[], [], [], []
 		episode_number = 1
-		reward_sum = 0
+		reward_sum_discount = 0
 		action = [0]
 		dlog_list = []
 		previous_observation = self.env.reset()
-		sum_reward_list = []
-		mean_sum_reward_list = []
+		sum_reward_discount_list = []
+		mean_sum_reward_discount_discount_list = []
 		# Training progress
 		time = 0		
 		while episode_number != EPISODE_NUMBER:
@@ -100,34 +106,35 @@ class Continuous_Agent:
 			mean, action[0] = self.step(previous_observation)
 			# give the observation and reward given an action.
 			observation, reward, done = self.env.step(action)
-			reward_sum += (reward * self.env.gamma() ** time)
+			reward_sum_discount += (reward * self.env.gamma() ** time)
+			reward_sum += reward
 			episode_state.append(previous_observation)
 			episode_action.append(action[0])
 			episode_mean.append(mean)
 			previous_observation = observation
 			if self.display:
-				self.Display.draw( self.env.bar.center, self.env.fruit.center, reward_sum, self.env.lives)
+				self.Display.draw( self.env.bar.center, self.env.fruit.center, reward_sum_discount, self.env.lives)
 			time += 1
-			# check if the episode of the game end or does not die after 10000 steps.
+			# check if the episode of the game end or does not die after TIME_LIMITE steps.
 			if done or time > TIME_LIMITE:
 				nb_element = 0
-				dlog = self.Dlog(reward_sum, episode_mean, episode_action, episode_state)
+				dlog = self.Dlog(reward_sum_discount, episode_mean, episode_action, episode_state)
 				dlog_list.append(dlog)
-				sum_reward_list.append(reward_sum)
+				sum_reward_discount_list.append(reward_sum_discount)
 				if episode_number % batch_size == 0:
 					self.update(dlog_list, batch_size)
-					sum_reward_array = np.asarray(sum_reward_list)
-					mean_sum_reward = np.mean(sum_reward_array, axis=0)
-					mean_sum_reward_list.append(mean_sum_reward)
+					sum_reward_discount_array = np.asarray(sum_reward_discount_list)
+					mean_sum_reward_discount = np.mean(sum_reward_discount_array, axis=0)
+					mean_sum_reward_discount_discount_list.append(mean_sum_reward_discount)
 					dlog_list = []
-					sum_reward_list = []
+					sum_reward_discount_list = []
 					print(self.theta)
-				reward_sum = 0
+				reward_sum_discount = 0
 				time = 0
 				episode_number +=1
 				episode_rewards, episode_state, episode_action, episode_mean =[], [], [], []
 				observation = self.env.reset()
-		return mean_sum_reward_list
+		return mean_sum_reward_discount_discount_list
 
 class Discrete_Agent:
 	def __init__(self, pretrained_model = None, learning_rate = 0.0001, std = 1, screen_size = None, display = True):
@@ -165,13 +172,13 @@ class Discrete_Agent:
 		action = int(round(action))
 		return mean, action
 		
-	def Dlog(self, reward_sum, episode_mean, episode_action, episode_state):
+	def Dlog(self, reward_sum_discount, episode_mean, episode_action, episode_state):
 		"""
 		return the sum of the derivative of the log policy multiply by the sum of reward.
 		"""
 		# compute the difference between the action and the mean of the gaussian model divide by the 
 		# variance and multiply by the sum of reward
-		dif = [(x1 - x2)/(self.std**2)*reward_sum for (x1, x2) in zip(episode_action, episode_mean)]
+		dif = [((x1 - x2)/(self.std**2))*reward_sum_discount for (x1, x2) in zip(episode_action, episode_mean)]
 		dlog = [a*b for a,b in zip(episode_state,dif)]
 		# sum for all weights
 		dlog_array = np.asarray(dlog)
@@ -185,7 +192,7 @@ class Discrete_Agent:
 		mean = np.mean(dlog, axis=0)
 		self.theta = [x1 + self.learning_rate*x2 for (x1, x2) in zip(self.theta, mean)]
 		
-	def evaluate(self, EPISODE_NUMBER = 50, TIME_LIMITE=1000):
+	def evaluate(self, EPISODE_NUMBER = 50, TIME_LIMITE=10000):
 		"""
 		Evaluate the model: return the mean of cumulative reward 
 		"""
@@ -193,31 +200,37 @@ class Discrete_Agent:
 		episode_number = 0
 		previous_observation = self.env.reset()
 		sum_reward_list = []
+		time = 0
 		while episode_number != EPISODE_NUMBER:
 			# give the mean and action given an obervation.
 			mean, action = self.step(previous_observation)
 			# give the observation and reward given an action.
 			observation, reward, done = self.env.step([action])
-			reward_sum += reward 
-			time = 0
+			reward_sum += reward
+			time += 1
 			if done or time > TIME_LIMITE:
-				sum_reward_list.append(reward_sum)
+				sum_reward_list.append(reward_sum_discount)
 				reward_sum = 0
 				episode_number += 1
-				previous_observation = self.env.reset()	
+				previous_observation = self.env.reset()
+				time = 0
+			previous_observation = observation
 		sum_reward_array = np.asarray(sum_reward_list)
 		return np.mean(sum_reward_array), np.std(sum_reward_array)
 		
-	def train(self, EPISODE_NUMBER = 2000, batch_size = 20, TIME_LIMITE = 1000):
+	def train(self, EPISODE_NUMBER = 2000, batch_size = 20, TIME_LIMITE = 10000):
+		"""
+		Train the model.
+		"""
 		# Init
 		episode_rewards, episode_state, episode_action, episode_mean =[], [], [], []
 		episode_number = 1
-		reward_sum = 0
+		reward_sum_discount = 0
 		action = [0]
 		dlog_list = []
 		previous_observation = self.env.reset()
-		sum_reward_list = []
-		mean_sum_reward_list = []
+		sum_reward_discount_list = []
+		mean_sum_reward_discount_discount_list = []
 		# Training progress
 		time = 0		
 		while episode_number != EPISODE_NUMBER:
@@ -226,39 +239,76 @@ class Discrete_Agent:
 			mean, action[0] = self.step(previous_observation)
 			# give the observation and reward given an action.
 			observation, reward, done = self.env.step(action)
-			reward_sum += (reward * self.env.gamma() ** time)
+			reward_sum_discount += (reward * self.env.gamma() ** time)
 			episode_state.append(previous_observation)
 			episode_action.append(action[0])
 			episode_mean.append(mean)
 			previous_observation = observation
 			if self.display:
-				self.Display.draw( self.env.bar.center, self.env.fruit.center, reward_sum, self.env.lives)
+				self.Display.draw( self.env.bar.center, self.env.fruit.center, reward_sum_discount, self.env.lives)
 			time += 1
-			# check if the episode of the game end or does not die after 10000 steps.
+			# check if the episode of the game end or does not die after TIME_LIMITE steps.
 			if done or time > TIME_LIMITE:
 				nb_element = 0
-				dlog = self.Dlog(reward_sum, episode_mean, episode_action, episode_state)
+				dlog = self.Dlog(reward_sum_discount, episode_mean, episode_action, episode_state)
 				dlog_list.append(dlog)
-				sum_reward_list.append(reward_sum)
+				sum_reward_discount_list.append(reward_sum_discount)
 				if episode_number % batch_size == 0:
 					self.update(dlog_list, batch_size)
-					sum_reward_array = np.asarray(sum_reward_list)
-					mean_sum_reward = np.mean(sum_reward_array, axis=0)
-					mean_sum_reward_list.append(mean_sum_reward)
+					sum_reward_discount_array = np.asarray(sum_reward_discount_list)
+					mean_sum_reward_discount = np.mean(sum_reward_discount_array, axis=0)
+					mean_sum_reward_discount_discount_list.append(mean_sum_reward_discount)
 					dlog_list = []
-					sum_reward_list = []
+					sum_reward_discount_list = []
 					print(self.theta)
-				reward_sum = 0
+				reward_sum_discount = 0
 				time = 0
 				episode_number +=1
 				episode_rewards, episode_state, episode_action, episode_mean =[], [], [], []
 				observation = self.env.reset()
-		return mean_sum_reward_list
+		return mean_sum_reward_discount_discount_list
 
 if __name__ == "__main__":
-	agent = Continuous_Agent()
-	print(agent.evaluate())
-	mean_sum_reward_list = agent.train()
+	import matplotlib.pyplot as plt
+	import numpy as np
+	continuous_agent = Continuous_Agent(display = False)
+	discrete_agent = Discrete_Agent(display = False)
+	continuous_mean,continuous_std = continuous_agent.evaluate()
+	continuous_mean_list = []
+	continuous_std_list = []
+	continuous_mean_list.append(continuous_mean)
+	continuous_std_list.append(continuous_std)
+	continuous_mean_sum_reward_discount_discount_list = []
+	discrete_mean,discrete_std = discrete_agent.evaluate()
+	discrete_mean_list = []
+	discrete_std_list = []
+	discrete_mean_list.append(discrete_mean)
+	discrete_std_list.append(discrete_std)
+	discrete_mean_sum_reward_discount_discount_list = []
+	for i in range(20):
+		print(i)
+		continuous_mean_sum_reward_discount_discount_list.append(continuous_agent.train())
+		continuous_mean,continuous_std = continuous_agent.evaluate()
+		continuous_mean_list.append(continuous_mean)
+		continuous_std_list.append(continuous_std)
+		discrete_mean_sum_reward_discount_discount_list.append(discrete_agent.train())
+		discrete_mean,discrete_std = discrete_agent.evaluate()
+		discrete_mean_list.append(discrete_mean)
+		discrete_std_list.append(discrete_std)
+	np.save("continuous_std_list",np.asarray(continuous_std_list))
+	np.save("continuous_mean_list",np.asarray(continuous_mean_list))
+	np.save("continuous_mean_sum_reward_discount_discount_list",np.asarray(continuous_mean_sum_reward_discount_discount_list))
+	np.save("discrete_std_list",np.asarray(continuous_std_list))
+	np.save("discrete_mean_list",np.asarray(continuous_mean_list))
+	np.save("discrete_mean_sum_reward_discount_discount_list",np.asarray(continuous_mean_sum_reward_discount_discount_list))
+	# Plot the data
+	plt.plot(continuous_mean_sum_reward_discount_discount_list, label='linear')
+
+	# Add a legend
+	plt.legend()
+
+	# Show the plot
+	plt.show()
 	print(agent.evaluate())
 	print(agent.theta)
 	
